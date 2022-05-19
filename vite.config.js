2e8567bda812cf77dd3700124d01b44dec912a80
infseeker <infseek@gmail.com>
2022-05-19 17:41:34 +0200

    feat: enable access on local network

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -12,6 +15,6 @@ export default defineConfig({
     },
   },
   css: {
-    devSourcemap: true
-  }
-})
+    devSourcemap: true,
+  },
+});
