4272d81583809dd1e39bd073365538e10838a321
infseeker <infseek@gmail.com>
2022-07-08 20:11:53 +0200

    fix: change icons of nav bar

--- src/components/user/UserNavigation.vue
+++ src/components/user/UserNavigation.vue
@@ -1,15 +1,15 @@
 <template>
   <nav class="layout-navbar navbar navbar-detached align-items-center bg-navbar-theme">
     <div class="navbar-nav-right d-flex align-items-center">
-      <router-link :to="{ name: 'home' }" class="navbar-nav align-items-center fs-4">
-        <i class='bx bxs-cat' style="font-size: 3rem;"></i>
+      <router-link :to="{ name: 'home' }" class="align-items-center home-link">
+        <i class='bx bx-infinite'></i>
       </router-link>
       <ul class="navbar-nav flex-row align-items-center ms-auto">
         <li v-if="!this.$user.isAuth">
-          <router-link :to="{ name: 'registration' }" class="btn btn-primary">Регистрация</router-link>
+          <router-link :to="{ name: 'registration' }" class="btn btn-primary" title="Регистрация"><i class='bx bx-user-plus'></i></router-link>
         </li>
         <li v-if="!this.$user.isAuth">
-          <router-link :to="{ name: 'login' }" class="btn btn-primary ms-2">Вход</router-link>
+          <router-link :to="{ name: 'login' }" class="btn btn-primary ms-2" title="Вход"><i class='bx bx-log-in'></i></router-link>
         </li>
         <li v-if="this.$user.isAuth && this.$user.isAdmin">
           <router-link :to="{ name: 'registration' }" class="btn btn-primary ms-2">Админка</router-link>
