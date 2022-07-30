0b774275ade2a851349287b085dcf9f93d8e869d
infseeker <infseek@gmail.com>
2022-07-30 19:21:05 +0200

    feat & fix: remove boxicons font and add single svg's

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import { loader } from './helpers/loader'
 
 import 'bootstrap';
 
-import 'boxicons/css/boxicons.min.css';
 import 'vue-loading-overlay/dist/vue-loading.css';
+import './assets/css/boxicons.css';
 import './assets/css/sneat-theme-core.min.css';
 import './assets/css/sneat-theme-default.min.css';
 import './assets/css/sneat-theme-custom.css';
